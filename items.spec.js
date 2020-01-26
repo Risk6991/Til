d4df35b65731afdaf6fb98f95fde987ba396b6c7
yuki_ito <ito@aracan.co.jp>
2020-01-27 04:35:11 +0900

    install prettier

--- items.spec.js
+++ items.spec.js
@@ -4,24 +4,23 @@ const $ = require("jquery");
 
 global.$ = $;
 
-describe("items.js",() => {
-    beforeEach(async() => {
-        const html = fs.readFileSync(path.resolve(__dirname,"server.html"));
-        document.body.innerHTML = html
+describe("items.js", () => {
+  beforeEach(async () => {
+    const html = fs.readFileSync(path.resolve(__dirname, "server.html"));
+    document.body.innerHTML = html;
 
-        require("./items.js");
-        document.dispatchEvent(new Event("DOMContentLoaded"));
+    require("./items.js");
+    document.dispatchEvent(new Event("DOMContentLoaded"));
 
-        //初期化を待つ
-        await new Promise(resolve => setTimeout(resolve,100));
-    });
+    //初期化を待つ
+    await new Promise(resolve => setTimeout(resolve, 100));
+  });
 
-    test("金額が計算される",async () => {
-        document.querySelector(".price").value = 100;
-        document.querySelector(".quantity").value = 200;
-        document.querySelector(".quantity").dispatchEvent(new Event("input"));
+  test("金額が計算される", async () => {
+    document.querySelector(".price").value = 100;
+    document.querySelector(".quantity").value = 200;
+    document.querySelector(".quantity").dispatchEvent(new Event("input"));
 
-        expect(document.querySelector(".total").textContent).toBe("20000")
-    });
-
+    expect(document.querySelector(".total").textContent).toBe("20000");
+  });
 });
