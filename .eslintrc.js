85733a3737b7eda40b8482ebe8c34cc80d89d0bc
yuki_ito <ito@aracan.co.jp>
2020-01-27 05:42:00 +0900

    過去に設定したrcファイルを移植

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        //JQuery対応
+        jquery: true,
+    },
+    extends: [
+        //JavaScript Standardガイドに沿う
+        "standard",
+        // `eslint-config-prettier` と `eslint-plugin-prettier` を有効化する
+        // ※ extends 配列の一番最後に配置すること
+        "plugin:prettier/recommended",
+    ],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+        //Google AnalyticsやGoogle Adsenseを除外する
+        googletag: true,
+        ga: true,
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        // ecmaVersion を指定してもこの記述を入れておかないと import/export 解析されない
+        sourceType: 'module',
+    },
+    rules: {},
+};
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // Prettier の設定を記述していきます
+    endOfLine: "lf",
+    semi: true,
+    printWidth: 80,
+    tabWidth: 4,
+    doubleQuote: true, //ダブルクォートに統一
+    // singleQuote: true,
+    trailingComma: "es5" //末尾のカンマをどうするか: es5に準拠させる。
+};
